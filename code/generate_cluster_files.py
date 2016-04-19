84775cb825e06b2c701b3fbcff6d9761d4c04e09
RuthAngus <ruth.angus@astro.ox.ac.uk>
2016-04-19 19:29:43 +0000

    adding generate_cluster_files.py

---
+++ code/generate_cluster_files.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+
+# NGC 5316: 90 G dwarfs, 240 K dwarfs, 1650 M dwarfs
+# NGC 2477: 90 G dwarfs, 240 K dwarfs, 1650 M dwarfs
+# IC 4651: 75 G dwarfs, 1500 M dwarfs.
+# G = 5200, 6000
+# K = 3700, 5200
+# M = 2400, 3700
+
+# data = np.vstack((logAges, bvs, logTeff, rmag))
+
