07dd98ef3079b32a9147be194c72f766e1a80b8c
RuthAngus <ruth.angus@astro.ox.ac.uk>
2016-04-19 19:36:35 +0000

    adding info

--- code/generate_cluster_files.py
+++ code/generate_cluster_files.py
@@ -1,13 +1,17 @@
 from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
+from teff_bv import teff2bv
 
 # NGC 5316: 90 G dwarfs, 240 K dwarfs, 1650 M dwarfs
+# Age = .170 Gyrs, distance = 1208 pc, dm = 10.410
 # NGC 2477: 90 G dwarfs, 240 K dwarfs, 1650 M dwarfs
+# Age = .822 Gyrs, distance = 1450, dm = 10.807
 # IC 4651: 75 G dwarfs, 1500 M dwarfs.
+# Age = 1.778 Gyrs, distance = 888, dm = 9.742
+
 # G = 5200, 6000
 # K = 3700, 5200
 # M = 2400, 3700
 
 # data = np.vstack((logAges, bvs, logTeff, rmag))
-
